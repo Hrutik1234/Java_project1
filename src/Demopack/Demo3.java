56ac5a1a34965c20d7695c211730128697eeaeec
root <hrutik1234@gmail.com>
2023-06-26 17:45:29 +0530

    New Demo2 Class created

---
+++ src/Demopack/Demo3.java
@@ -0,0 +1,11 @@
+package Demopack;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i=10;
+     System.out.println("I valuse is="+i);
+	}
+
+}
